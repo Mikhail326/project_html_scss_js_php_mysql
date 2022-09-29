beaf06607331601c557ecd0de17c7087a91bbb8c
Mikhail326 <misha.stelmakh1995@gmail.com>
2022-09-29 15:23:20 +0300

    completed packages-section

--- src/js/main.js
+++ src/js/main.js
@@ -1,41 +1,60 @@
 const hamburger = document.querySelector('#menu-hamburger')
 const nav = document.querySelector('.header')
+const packages = document.querySelectorAll('.packages .box-container__item')
+const btnShowPackages = document.querySelector('#show-packages')
+let courent = 6;
 
 const handleHamburger = () => {
-    hamburger.classList.toggle('active')
-    nav.classList.toggle('active')
+  hamburger.classList.toggle('active')
+  nav.classList.toggle('active')
 }
 
 
 const swiperHome = new Swiper(".home__slider", {
-    loop: true,
-    autoplay: { delay: 2000},
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    }
+  loop: true,
+  autoplay: { delay: 2000 },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  }
 })
 
 var swiperReviews = new Swiper(".reviews__slider", {
-    loop: true,
-    spaceBetween: 20,
-    autoHeight: true,
-    grabCursor: true,
-    pagination: {
-        el: ".swiper-pagination",
-        dynamicBullets: true,
-      },
-    breakpoints: {
-      640: {
-        slidesPerView: 1,
-      },
-      768: {
-        slidesPerView: 2,
-      },
-      1024: {
-        slidesPerView: 3,
-      },
+  loop: true,
+  spaceBetween: 20,
+  autoHeight: true,
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
     },
-  });
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+const showPackages = () => {
+  for (let i = courent; i < courent + 3; i++) {
+    if(packages[i] === undefined) {
+      btnShowPackages.style.display = 'none'
+      break;
+    }
+    packages[i].style.display = 'inline-block'
+   
+  }
+  courent += 3;
+  if(packages.length <= courent) {
+    btnShowPackages.style.display = 'none'
+  }
+}
 
 hamburger.addEventListener('click', handleHamburger)
+btnShowPackages.addEventListener('click', showPackages)
