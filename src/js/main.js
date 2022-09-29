131f132f45b63428d0647697fdf1226d05b82bb6
Mikhail326 <misha.stelmakh1995@gmail.com>
2022-09-29 13:17:35 +0300

    completed reviews-section

--- src/js/main.js
+++ src/js/main.js
@@ -7,12 +7,35 @@ const handleHamburger = () => {
 }
 
 
-const swiper = new Swiper(".home__slider", {
+const swiperHome = new Swiper(".home__slider", {
     loop: true,
+    autoplay: { delay: 2000},
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     }
 })
+
+var swiperReviews = new Swiper(".reviews__slider", {
+    loop: true,
+    spaceBetween: 20,
+    autoHeight: true,
+    grabCursor: true,
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+      },
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
 
 hamburger.addEventListener('click', handleHamburger)
